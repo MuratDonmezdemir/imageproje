f52dd9f7fa305f804c3c4df11c21a4eeabdf4bd9
Murat Dönmezdemir <testingdnmzdmr@gmail.com>
2025-05-12 13:26:58 +0300

    İlk commit

---
+++ Desktop/AlummiPlaywright/P01_Helloworld.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+console .log ('hello murat')
+console.log("aile candır")
+// Console  "Hello  World"yazdıralım 
+// System.out.println("hello world")
+console .log ('"Canim Birtanem"')
+/* tek satır console.log ile 
+hello 
+world 
+yazdırın*/
+console.log ('"Hello/World"')
