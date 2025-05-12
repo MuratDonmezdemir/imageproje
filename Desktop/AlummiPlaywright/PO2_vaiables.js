8cf8483fedb8994d1951e4c13720b3ca7cb2415b
Murat Dönmezdemir <testingdnmzdmr@gmail.com>
2025-05-12 18:45:13 +0300

    VAriables

---
+++ Desktop/AlummiPlaywright/PO2_vaiables.js
@@ -0,0 +1,28 @@
+
+// java scripte 3 farklı değişken tanımlanır
+// let ve var ile oluşturulan variable lar
+
+//atanan degerler değişebilir özellikdedir  
+//const ile olusturulan  variable lara atana deger değistirilemez 
+let firstname ="Murat" ; // string
+var lastname = "Dönmezdemir"; // string
+let age =44 ;// number 
+const İdNumber =12345678; // değiştirilemez 
+console.log("adı :"+firstname);
+// PO2_variables.js dosyasını düzenlenmiş hali
+
+console.log("Soyadı: " + lastname);
+
+console.log("yaşı:"+age);
+console.log("kimliknosu:"+İdNumber);
+console.log("-----------------");
+
+
+firstname=" Arzum";
+lastname= "dönmezdemir";
+age =40;
+
+console.log("Soyadi: " + lastname);
+
+console.log("yaşi :"+age);
+console.log("kimliknosu:"+İdNumber);
